9cd558fd31b0c9a56fd6472df635bc680290fccd
krsamir <samir1741012321@gmail.com>
2020-06-28 20:35:53 +0530

    updated search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>Robo Friends</h1>
-      <SearchBox/>
-      <CardList robots={robots} />
-    </div>
-  );
-};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.first_name.toLowerCase().includes(this.state.searchfield.toLowerCase);
+    });
+  }
+  //   about error:
+  //   the value of this is now refering to the app because the event happened in the input
+  //   the value of this is well the input and input does not have states.robot.
+  //   to overcome error use arrow functions.
+  render() {
+    return (
+      <div className="tc">
+        <h1>Robo Friends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        {/* <CardList robots={robots} /> */}
+        <CardList robots={this.state.robots} />
+      </div>
+    );
+  }
+}
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className="pa2">
       <input
-      className='pa3 ba b--green bg-lightest-blue' 
-      type="Search" 
-      placeholder="Search Robots" />
+        className="pa3 ba b--green bg-lightest-blue"
+        type="Search"
+        placeholder="Search Robots"
+        onChange = {searchChange}
+      />
     </div>
   );
 };
